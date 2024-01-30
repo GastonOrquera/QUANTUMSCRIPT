ac6698fb76f5a7d45de658acc66dfdef731ff5c0
GastonOrquera <gastonorquera29@gmail.com>
2024-01-30 05:30:40 -0300

    QUANTUMSCRIPT

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
+// var hamburger = document.querySelector('.hamburger');
+
+// var nav = document.querySelector('nav');
+
+// var navLinks = document.querySelectorAll('nav a');
+
+
+
+// hamburger.onclick = function () {
+//     nav.classList.toggle('active');
+
+// }
+
+// navLinks.forEach(function (link) {
+//     link.addEventListener('click', function () {
+//         nav.classList.remove('active');
+//     });
+// });
+
+
+
+
+
+
+
 var hamburger = document.querySelector('.hamburger');
-
 var nav = document.querySelector('nav');
-
 var navLinks = document.querySelectorAll('nav a');
 
-var title = document.querySelector('.titleMain');
-
 hamburger.onclick = function () {
+    hamburger.classList.toggle('active');
     nav.classList.toggle('active');
-    title.classList.toggle('active');
-}
+};
 
 navLinks.forEach(function (link) {
     link.addEventListener('click', function () {
+        hamburger.classList.remove('active');
         nav.classList.remove('active');
     });
 });
+
 
 ScrollReveal().reveal('.scroll-reveal', {
     origin: 'top',
@@ -32,3 +55,4 @@ ScrollReveal().reveal('.scroll-revealLeft', {
     easing: 'cubic-bezier(0.5,0,0,1)',
     interval: 100
 });
+
